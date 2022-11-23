86821f186188c744843e983de18f7c6d6675d3d1
shaozam47 <bjayesh47@gmail.com>
2022-11-23 21:00:50 +0530

    Second Commit

---
+++ app/src/main/java/com/example/teamup/activities/BaseActivity.kt
@@ -0,0 +1,55 @@
+package com.example.teamup.activities
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.teamup.R
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.dialog_progress.*
+
+
+open class BaseActivity : AppCompatActivity() {
+
+    private var doubleBackToExitPressedOnce = false
+
+    private lateinit var mProgressDialog: Dialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+    }
+
+    fun showProgressDialog(text: String) {
+        mProgressDialog = Dialog(this)
+        mProgressDialog.setContentView(R.layout.dialog_progress)
+        mProgressDialog.tv_progress_text.text = text
+        mProgressDialog.show()
+    }
+
+    fun hideProgressDialog(){
+        mProgressDialog.dismiss()
+    }
+
+    fun getCurrentUserID(): String {
+        return FirebaseAuth.getInstance().currentUser!!.uid
+    }
+
+    fun doubleBackToExit() {
+        if(doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this,
+        "Please Click Back Once Again To Exit",
+        Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed({doubleBackToExitPressedOnce = false}, 2000)
+    }
+
+}
--- app/src/main/java/com/example/teamup/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/teamup/activities/SignUpActivity.kt
@@ -1,12 +1,15 @@
 package com.example.teamup.activities
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.example.teamup.R
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -28,5 +31,44 @@ class SignUpActivity : AppCompatActivity() {
         }
 
         toolbar.setNavigationOnClickListener{onBackPressed()}
+
+        btn_sign_up.setOnClickListener {
+            registerUser() }
+    }
+
+    private fun registerUser() {
+        val name: String = et_name.text.toString().trim{it <= ' '}
+        val email: String = et_email.text.toString().trim{it <= ' '}
+        val password: String = et_password.text.toString().trim(){it <= ' '}
+
+        if(validateForm(name, email, password)) {
+            Toast.makeText(this@SignUpActivity,
+                "Now We Can Register A User",
+            Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validateForm(name: String, email: String, password:String): Boolean {
+        return when {
+            TextUtils.isEmpty(name)->{
+                showError("Please Enter A Name")
+                false
+            }
+            TextUtils.isEmpty(email)->{
+                showError("Please Enter A Email")
+                false
+            }
+            TextUtils.isEmpty(password)-> {
+                showError("Please Enter A Password")
+                false
+            }
+            else -> {
+                true
+            }
+        }
+    }
+
+    private fun showError(message: String) {
+        Toast.makeText(this@SignUpActivity, message, Toast.LENGTH_LONG).show()
     }
 }
