6173e45c932fae4ddb6a35daddeb4cfe2c7776c6
shaozam47 <bjayesh47@gmail.com>
2022-11-24 21:20:22 +0530

    Fifth Commit

--- app/src/main/java/com/example/teamup/activities/MainActivity.kt
+++ app/src/main/java/com/example/teamup/activities/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.teamup.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.core.view.GravityCompat
+import com.bumptech.glide.Glide
 import com.example.teamup.R
+import com.example.teamup.firebase.FireStoreClass
+import com.example.teamup.models.User
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -20,6 +22,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 
         setUpActionBar()
         nav_view.setNavigationItemSelectedListener(this)
+        FireStoreClass().loadUserData(this)
     }
 
     private fun setUpActionBar() {
@@ -52,7 +55,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.nav_my_profile -> {
-                Toast.makeText(this@MainActivity, "My Profile", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, MyProfileActivity::class.java))
             }
             R.id.nav_sign_out -> {
                 FirebaseAuth.getInstance().signOut()
@@ -64,5 +67,18 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         }
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    fun updateNavigationUserDetails(user: User?) {
+        if (user != null) {
+            Glide
+                .with(this)
+                .load(user.image)
+                .centerCrop()
+                .placeholder(R.drawable.ic_user_place_holder)
+                .into(nav_user_image)
+
+            tv_username.text = user.name
+        }
     }
 }
